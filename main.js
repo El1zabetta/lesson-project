3dae0aeb16b0cad4e8df1b7a26677c1f7d0ea2bc
El1zabetta <elizabettahirov@gmail.com>
2024-04-24 21:38:53 +0600

    added edit, details, funcions

--- main.js
+++ main.js
@@ -19,14 +19,41 @@ let inpFirstPrice = document.getElementById("add-price-1");
 let inpSecondPrice = document.getElementById("add-price-2");
 let addProductBtn = document.getElementById("add-btn");
 
+//? На кнопку навесили события чтобы открыть модалку
 openAddFormBtn.addEventListener("click", () => {
   addModal.style.display = "flex";
 });
 
+//?начало details
+let closeDetailsBtn = document.getElementById("close-details");
+let detailsParent = document.getElementById("details");
+let detailsInner = document.getElementById("details-inner");
+closeDetailsBtn.addEventListener("click", () => {
+  detailsParent.style.display = "none";
+});
+
+//? Закрытие модалки при клике на крестик внутри нее
 closeAddFormBtn.addEventListener("click", () => {
   addModal.style.display = "none";
 });
 
+//! edit
+let editModal = document.getElementById("edit-modal");
+let editModalInner = document.getElementById("edit-modal-inner");
+let closeEditModalBtn = document.getElementById("close-edit-modal");
+
+//? достаем форму добавления
+let inpImageEdit = document.getElementById("edit-img");
+let inpDescriptionEdit = document.getElementById("edit-description");
+let inpFirstPriceEdit = document.getElementById("edit-price-1");
+let inpSecondPriceEdit = document.getElementById("edit-price-2");
+let editProductBtn = document.getElementById("edit-btn");
+
+closeEditModalBtn.addEventListener("click", () => {
+  editModal.style.display = "none";
+});
+
+//? // ? При клике на кнопку внутри модалки, собираем данные с инпута в объект и делаем запрос на добавление продукта, после скрываем модалку
 addProductBtn.addEventListener("click", async () => {
   let product = {
     image: inpImage.value,
@@ -34,21 +61,32 @@ addProductBtn.addEventListener("click", async () => {
     first_price: inpFirstPrice.value,
     second_price: inpSecondPrice.value,
   };
-
-  await fetch(API, {
-    method: "POST",
-    body: JSON.stringify(product),
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
-  });
+  //? отправляем данные
+  if (
+    inpImage.value.trim() &&
+    inpDescription.value.trim() &&
+    inpFirstPrice.value.trim() &&
+    inpSecondPrice.value.trim()
+  ) {
+    await fetch(API, {
+      method: "POST",
+      body: JSON.stringify(product),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+    });
+  } else {
+    alert("Заполните поля!");
+  }
   addModal.style.display = "none";
 });
 
+//? создаем асинхронную функцию для того чтобы достать данные с бэкенда и затем отобразить
 async function showProducts() {
   let products = await fetch(API).then((res) => res.json());
 
   productsList.innerHTML = "";
+  //? перебираем массив для того чтобы создать div для карточки и поместить его в product list т.е в родителя карточек
 
   products.forEach((product, id) => {
     let div = document.createElement("div");
@@ -63,8 +101,76 @@ async function showProducts() {
     </div>
     `;
 
+    //! details
+    div.addEventListener("click", () => {
+      detailsInner.innerHTML = "";
+      detailsParent.style.display = "flex";
+
+      detailsInner.innerHTML = `
+     <img src="${product.image}"/>
+     <div class="details-text" id="details-text">
+      <p>${product.description}</p>
+       <div class="card-price">
+         <p>${product.first_price} сом</p>
+         <p>${product.second_price} сом</p>
+       </div>
+     </div>`;
+      let detailsText = document.getElementById("details-text");
+      let btnDelete = document.createElement("button");
+      btnDelete.innerHTML = "Удалить продукт";
+
+      btnDelete.addEventListener("click", (e) => deleteProduct(product.id, e));
+
+      //! Создаем кнопку для редактирования внутри details
+      let editBtn = document.createElement("button");
+      editBtn.innerHTML = "Редактировать продукт";
+      editBtn.addEventListener("click", () => {
+        editModal.style.display = "flex";
+        editModal.style.zIndex = "2";
+        editProduct(product);
+      });
+      detailsText.append(btnDelete, editBtn);
+    });
+
+    //! edit
+
     productsList.append(div);
   });
 }
-
+//? Вызываем эту функцию 1 раз в глобальной области видимости для того чтобы при загрузке страницы сразу отобразились данные
 showProducts();
+
+async function deleteProduct(id, event) {
+  event.stopPropagation();
+  await fetch(`${API}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+  detailsParent.style.display = "none";
+}
+
+async function editProduct(product) {
+  console.log("product: ", product);
+  inpImageEdit.value = product.image;
+  inpDescriptionEdit.value = product.description;
+  inpFirstPriceEdit.value = product.first_price;
+  inpSecondPriceEdit.value = product.second_price;
+
+  editProductBtn.addEventListener("click", async () => {
+    let newProduct = {
+      image: inpImageEdit.value,
+      description: inpDescriptionEdit.value,
+      first_price: inpFirstPriceEdit.value,
+      second_price: inpSecondPriceEdit.value,
+    };
+    await fetch(`${API}/${product.id}`, {
+      method: "PATCH",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+    });
+  });
+}
